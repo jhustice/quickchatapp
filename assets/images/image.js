40362ba90019f020f9bedec829400b3a60fa1f3f
Jhustice <justicechime22@gmail.com>
2024-07-27 13:42:53 +0100

    Initial commit

---
+++ assets/images/image.js
@@ -0,0 +1,8 @@
+import Plus from "./plus.svg";
+import Search from "./search.svg";
+import Profile1 from "./profile1.jpg";
+import Profile2 from "./profile2.jpg";
+import Profile3 from "./profile3.jpg";
+import Blank from "./blank.png";
+
+export {  Plus, Search, Profile1,  Profile2,  Profile3, Blank, };
